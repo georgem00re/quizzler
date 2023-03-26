d0881bef69eda9729e0daba0135a86284c10d3e7
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-03-26 11:12:20 +0100

    setup vitest in react-frontend

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -11,5 +11,9 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts"
   }
 })
