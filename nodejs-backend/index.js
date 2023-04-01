5e3c394b346172dacde57f886fd012d644bb0425
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-04-01 18:32:50 +0100

    initial changes

---
+++ nodejs-backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.get("/", () => {
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
