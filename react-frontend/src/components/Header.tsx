646f761365f6e185171994fbe53aa5834a5de8fc
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-04-07 10:50:39 +0100

    finishing changes

--- react-frontend/src/components/Header.tsx
+++ react-frontend/src/components/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Logo from "./Logo.tsx";
-
 const TITLE = "quizzler.io"
 
 export default function Header() {
     return (
-        <div class="columns is-flex is-vcentered">
+        <div class="columns is-flex is-vcentered is-centered">
+            <h1 class="title is-1 has-text-dark has-text-centered" style={style}>{TITLE}</h1>
             <Logo/>
-            <h1 class="title is-1 has-text-dark has-text-centered">{TITLE}</h1>
         </div>
     )
+}
+
+const style = {
+    margin: "0px",
+    marginRight: "5px"
 }
