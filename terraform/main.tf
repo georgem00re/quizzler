38550a5cd317e3ae25efa945f0daeaa139ed2ced
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-05-23 20:50:52 +0100

    update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,11 @@
 
 provider "aws" {
-	region = "eu-west-2"
+	region = var.aws_region
 	access_key = "my_access_key"
 	secret_key = "my_secret_key"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.access_control_list
 }
