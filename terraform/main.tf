ee6fa52a006df5d792ae8f527964b1a848903aec
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-06-09 21:32:34 +0100

    add s3 bucket ownership controls

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,13 @@ resource "aws_s3_bucket" "s3_bucket" {
     bucket_prefix = var.bucket_prefix
 }
 
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
+    bucket = aws_s3_bucket.s3_bucket.id
+    rule {
+        object_ownership = "ObjectWriter"
+    }
+}
+
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
     bucket = aws_s3_bucket.s3_bucket.id
     acl = "public-read"
@@ -19,6 +26,14 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket_website_configuration"
     index_document {
         suffix = "index.html"
     }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+    bucket = aws_s3_bucket.s3_bucket.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
