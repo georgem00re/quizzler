010560224fe517635ea3692f1f904a3df3118222
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-06-12 20:21:43 +0100

    final change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 
 provider "aws" {
 	region = "eu-west-2"
-	profile = "default"
+	profile = "root"
 }
 
 module "s3_bucket" {
 	source = "./modules/s3_bucket"
-	bucket_prefix = "quizzler_react_app_"
+	bucket_prefix = "quizzler-react-app-"
 }
