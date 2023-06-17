592b0df0eb5a39a58d24369f038dc9fdc93f4616
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-06-17 19:42:59 +0100

    additional changes

--- terraform/modules/aws_iam_user/main.tf
+++ terraform/modules/aws_iam_user/main.tf
@@ -2,3 +2,7 @@
 resource "aws_iam_user" "this" {
 	name = var.name
 }
+
+resource "aws_iam_access_key" "this" {
+	user = aws_iam_user.this.name
+}
--- terraform/modules/aws_iam_user/outputs.tf
+++ terraform/modules/aws_iam_user/outputs.tf
@@ -2,3 +2,11 @@
 output "arn" {
 	value = aws_iam_user.this.arn
 }
+
+output "secret_access_key" {
+	value = aws_iam_access_key.this.secret
+}
+
+output "access_key_id" {
+	value = aws_iam_access_key.this.id
+}
