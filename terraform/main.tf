0714074a267e9ff8cd0e76dd2272f0e65684c2d2
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-06-17 17:15:43 +0100

    additional changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,21 +4,22 @@ provider "aws" {
 	profile = "root"
 }
 
-# module "iam_user" {
-# 	source = "./modules/aws_iam_user"
-# 	name = "quizzler_github_actions"
-# }
-
-# module "iam_role" {
-# 	source = "./modules/aws_iam_role"
-# 	name = "update_s3_bucket"
-# 	description = "Update S3 bucket"
-# 	assumeBy = module.iam_user.arn
-# 	action = ["s3:PutObject", "s3:DeleteObject"]
-# 	resource = [module.s3_bucket.arn]
-# }
-
 module "s3_bucket" {
 	source = "./modules/aws_s3_bucket"
 	bucket_prefix = "quizzler-react-app-"
+}
+
+module "iam_user" {
+	source = "./modules/aws_iam_user"
+	name = "quizzler_github_actions"
+}
+
+module "iam_role" {
+	source = "./modules/aws_iam_role"
+	name = "update_s3_bucket"
+	description = "Update S3 bucket"
+	assumeBy = module.iam_user.arn
+	action = ["s3:PutObject", "s3:DeleteObject"]
+	resource = [module.s3_bucket.arn]
+	depends_on = [module.s3_bucket, module.iam_user]
 }
