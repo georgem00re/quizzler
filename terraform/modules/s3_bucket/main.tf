9f6be17d9af43563b3257bca3e665bbf56bdf00f
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-06-17 10:07:26 +0100

    initial changes

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -10,11 +10,6 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
     }
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-    bucket = aws_s3_bucket.s3_bucket.id
-    acl = "public-read"
-}
-
 resource "aws_s3_bucket_website_configuration" "s3_bucket_website_configuration" {
     bucket = aws_s3_bucket.s3_bucket.id
 
@@ -29,6 +24,12 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
     block_public_policy = false
     ignore_public_acls = false
     restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+    bucket = aws_s3_bucket.s3_bucket.id
+    acl = "public-read"
+    depends_on = [aws_s3_bucket_public_access_block.s3_bucket_public_access_block]
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
