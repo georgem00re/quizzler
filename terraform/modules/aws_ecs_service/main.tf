d69d88b69ae814e29d598d7686b3094135f20ff3
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-07-01 20:01:25 +0100

    add network configurations to aws_ecs_service

--- terraform/modules/aws_ecs_service/main.tf
+++ terraform/modules/aws_ecs_service/main.tf
@@ -5,4 +5,21 @@ resource "aws_ecs_service" "this" {
 	task_definition = var.task_definition_arn
 	desired_count = var.desired_count
 	launch_type = "FARGATE"
+
+	network_configuration {
+		subnets = ["${aws_default_subnet.eu-west-2a.id}", "${aws_default_subnet.eu-west-2b.id}", "${aws_default_subnet.eu-west-2c.id}"]
+		assign_public_ip = true
+	}
+}
+
+resource "aws_default_subnet" "eu-west-2a" {
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_default_subnet" "eu-west-2b" {
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_default_subnet" "eu-west-2c" {
+  availability_zone = "eu-west-2c"
 }
