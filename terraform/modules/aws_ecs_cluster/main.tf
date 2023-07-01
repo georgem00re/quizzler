599d94ffc617cb09b58031c2f06e6ad28b65e446
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-07-01 16:46:31 +0100

    additional changes

--- terraform/modules/aws_ecs_cluster/main.tf
+++ terraform/modules/aws_ecs_cluster/main.tf
@@ -2,18 +2,3 @@
 resource "aws_ecs_cluster" "this" {
 	name = var.cluster_name
 }
-
-resource "aws_ecs_task_definition" "this" {
-	family = var.task_name
-	container_definitions = jsonencode([{
-		name = "first"
-		image = var.aws_ecr_repository_url
-		essential = true
-		portMappings = [{
-			containerPort = 5000
-			hostPort = 5000
-		}]
-		memory = 512
-		cpu = 256
-	}])
-}
--- terraform/modules/aws_ecs_cluster/variables.tf
+++ terraform/modules/aws_ecs_cluster/variables.tf
@@ -2,11 +2,3 @@
 variable "cluster_name" {
 	type = string
 }
-
-variable "task_name" {
-	type = string
-}
-
-variable "aws_ecr_repository_url" {
-	type = string
-}
