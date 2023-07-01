bb9950d995a6987fe6f4dde06db856c215e4198d
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-07-01 17:52:52 +0100

    Provision an ECS service to run Quizzler task definition

---
+++ terraform/modules/aws_ecs_service/variables.tf
@@ -0,0 +1,16 @@
+
+variable "name" {
+	type = string
+}
+
+variable "cluster_id" {
+	type = string
+}
+
+variable "task_definition_arn" {
+	type = string
+}
+
+variable "desired_count" {
+	type = number
+}
